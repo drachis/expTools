65ba0d6125e365a32def4ef080e3b7c922833eab
Drachis <drachis@gmail.com>
2013-02-28 18:57:30 -0800

    added some simple blender python calls to get image object names, test framework needs repairs

--- blender/save_images.py
+++ blender/save_images.py
@@ -1,4 +1,12 @@
+import bpy
+def findImages():
+    images = []
+    for obj in bpy.data.images: 
+        images.append(obj.name)
+        print ("found an image", obj.name)
+    return images
 def saveImage():
-	return None
+    return None
 if __name__ == "__main__":
-	assert(saveImage() == None)
+    assert(saveImage() == None)
+    findImages()
