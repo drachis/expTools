4c76775151132d421635edeae483ba432750ec78
Toli <drachis+git@gmail.com>
2014-11-17 09:11:36 -0800

    switched from python 2 to 3 replaced urllib2 with urllib and cStringIO for shutil

--- python/dl_images.py
+++ python/dl_images.py
@@ -6,29 +6,36 @@ Created on Thu Nov 06 17:16:02 2014
 """
 
 #http://promo.na.leagueoflegends.com/sru-map-assets/6/47/39.png
-import urllib2
-import cStringIO
+import urllib.request
+import shutil
+import io
 from PIL import Image
 
 def downloadImages():
     files = {}
     for h in range(0,47):
         for v in range(0,40):
-            filename = ("C:/Riot Games/new_Sr_{0}_{1}.png".format(h,v))
-            files[filename] = (cStringIO.StringIO(urllib2.urlopen("http://promo.na.leagueoflegends.com/sru-map-assets/6/{0}/{1}.png".format(h,v)).read()))
-        print h
+            filename = ("H:/Projects/Python/Downloaded/new_Sr_{0}_{1}.png".format(h,v))
+            
+            url = "http://promo.na.leagueoflegends.com/sru-map-assets/6/{0}/{1}.png".format(h,v)
+            with urllib.request.urlopen(url) as response, open(filename,'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+                files[filename] = out_file
+        print( h)
     for filename in files:
-        image_info = Image.open(files[filename])
-        image_info.save(filename)
+        try:
+            in_file = Image.save(files[filename])
+        except:
+            print("cannot save file", filename)
         files[filename].close()
 
-def compositeImage(h = horizontal, v = Vertical, files):
-    files[]
+def compositeImage(h, v, files):
+    
     image = Image.image.new("RGB", (h,v) ,"Black")
     
  
 if __name__ == "__main__":
-    #downloadIMages()
+    downloadImages()
 
-    for h in new_Sr_{0}_{1}
+    #for h in new_Sr_{0}_{1}
            
