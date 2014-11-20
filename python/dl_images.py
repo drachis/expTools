b215dcdeff6b2ab7d137d0d3b495c7f83434b43b
Toli <drachis+git@gmail.com>
2014-11-20 09:58:32 -0800

    image download and composition works

--- python/dl_images.py
+++ python/dl_images.py
@@ -11,12 +11,11 @@ import shutil
 import io
 from PIL import Image
 
-def downloadImages():
+def downloadImages(_h = 47, _v = 40):
     files = {}
-    for h in range(0,47):
-        for v in range(0,40):
-            filename = ("H:/Projects/Python/Downloaded/new_Sr_{0}_{1}.png".format(h,v))
-            
+    for h in range(0,_h):
+        for v in range(0,_v):
+            filename = ("./data/new_Sr_{0}_{1}.png".format(h,v))            
             url = "http://promo.na.leagueoflegends.com/sru-map-assets/6/{0}/{1}.png".format(h,v)
             with urllib.request.urlopen(url) as response, open(filename,'wb') as out_file:
                 shutil.copyfileobj(response, out_file)
@@ -27,15 +26,33 @@ def downloadImages():
             in_file = Image.save(files[filename])
         except:
             print("cannot save file", filename)
+        in_file.close()
         files[filename].close()
+    return files
 
-def compositeImage(h, v, files):
+def compositeImage(h, v, size, files):    
+    image = Image.new("RGB", (h*size[0],v*size[1]) ,"Black")
+    for block in files:
+        for index in block:
+            sub = Image.open(block[index])
+            box = (index[0]*size[0],
+                   v*size[1]-(index[1]+1)*size[1],
+                   #index[0]*size[0]+size[0],
+                   #index[1]*size[1]+size[1]
+                   )
+            image.paste(sub,box)
+            image.save("./data/new_Sr_comp.png")
+def determineCompSize(files):
+    size = []
+    for file in files:
+        return Image.open(files[file]).size
     
-    image = Image.image.new("RGB", (h,v) ,"Black")
-    
- 
 if __name__ == "__main__":
-    #downloadImages()
-
+    _h = 47
+    _v = 40
+    downloadImages(_h,_v)
+    determineCompSize({'filename':"./data/new_Sr_1_1.png"})
+    filePaths = [{(h,v):"./data/new_Sr_{0}_{1}.png".format(h,v)} for h in range(_h) for v in range(_v)]
+    compositeImage(_h, _v, size=(256,256), files=filePaths)
     #for h in new_Sr_{0}_{1}
            
